5921d0d6a2060b755d991eb454fd8ef43fd0b33e
Christopher Baldwin <chrisbaldwinj89@gmail.com>
2022-02-23 01:15:17 -0500

    Add project files.

---
+++ YTSDesktop/JSONMaster.cs
@@ -0,0 +1,171 @@
+﻿// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Numerics;
+
+
+public class movie
+{
+    public List<Movy> Movy { get; set; }
+}
+
+public class Torrent
+{
+    [JsonProperty("url")]
+    public string Url { get; set; }
+
+    [JsonProperty("hash")]
+    public string Hash { get; set; }
+
+    [JsonProperty("quality")]
+    public string Quality { get; set; }
+
+    [JsonProperty("type")]
+    public string Type { get; set; }
+
+    [JsonProperty("seeds")]
+    public int Seeds { get; set; }
+
+    [JsonProperty("peers")]
+    public int Peers { get; set; }
+
+    [JsonProperty("size")]
+    public string Size { get; set; }
+
+    [JsonProperty("size_bytes")]
+    public long SizeBytes { get; set; }
+
+    [JsonProperty("date_uploaded")]
+    public string DateUploaded { get; set; }
+
+    [JsonProperty("date_uploaded_unix")]
+    public int DateUploadedUnix { get; set; }
+}
+
+public class Movy
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("url")]
+    public string Url { get; set; }
+
+    [JsonProperty("imdb_code")]
+    public string ImdbCode { get; set; }
+
+    [JsonProperty("title")]
+    public string Title { get; set; }
+
+    [JsonProperty("title_english")]
+    public string TitleEnglish { get; set; }
+
+    [JsonProperty("title_long")]
+    public string TitleLong { get; set; }
+
+    [JsonProperty("slug")]
+    public string Slug { get; set; }
+
+    [JsonProperty("year")]
+    public int Year { get; set; }
+
+    [JsonProperty("rating")]
+    public double Rating { get; set; }
+
+    [JsonProperty("runtime")]
+    public int Runtime { get; set; }
+
+    [JsonProperty("genres")]
+    public List<string> Genres { get; set; }
+
+    [JsonProperty("summary")]
+    public string Summary { get; set; }
+
+    [JsonProperty("description_full")]
+    public string DescriptionFull { get; set; }
+
+    [JsonProperty("synopsis")]
+    public string Synopsis { get; set; }
+
+    [JsonProperty("yt_trailer_code")]
+    public string YtTrailerCode { get; set ; }
+
+    [JsonProperty("language")]
+    public string Language { get; set; }
+
+    [JsonProperty("mpa_rating")]
+    public string MpaRating { get; set; }
+
+    [JsonProperty("background_image")]
+    public string BackgroundImage { get; set; }
+
+    [JsonProperty("background_image_original")]
+    public string BackgroundImageOriginal { get; set; }
+
+    [JsonProperty("small_cover_image")]
+    public string SmallCoverImage { get; set; }
+
+    [JsonProperty("medium_cover_image")]
+    public string MediumCoverImage { get; set; }
+
+    [JsonProperty("large_cover_image")]
+    public string LargeCoverImage { get; set; }
+
+    [JsonProperty("state")]
+    public string State { get; set; }
+
+    [JsonProperty("torrents")]
+    public List<Torrent> Torrents { get; set; }
+
+    [JsonProperty("date_uploaded")]
+    public string DateUploaded { get; set; }
+
+    [JsonProperty("date_uploaded_unix")]
+    public int DateUploadedUnix { get; set; }
+}
+
+public class Data
+{
+    [JsonProperty("movie_count")]
+    public int MovieCount { get; set; }
+
+    [JsonProperty("limit")]
+    public int Limit { get; set; }
+
+    [JsonProperty("page_number")]
+    public int PageNumber { get; set; }
+
+    [JsonProperty("movies")]
+    public List<Movy> Movies { get; set; }
+}
+
+public class Meta
+{
+    [JsonProperty("server_time")]
+    public int ServerTime { get; set; }
+
+    [JsonProperty("server_timezone")]
+    public string ServerTimezone { get; set; }
+
+    [JsonProperty("api_version")]
+    public int ApiVersion { get; set; }
+
+    [JsonProperty("execution_time")]
+    public string ExecutionTime { get; set; }
+}
+
+public class Root
+{
+    [JsonProperty("status")]
+    public string Status { get; set; }
+
+    [JsonProperty("status_message")]
+    public string StatusMessage { get; set; }
+
+    [JsonProperty("data")]
+    public Data Data { get; set; }
+
+    [JsonProperty("@meta")]
+    public Meta Meta { get; set; }
+}
+
---
+++ YTSDesktop/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace YTSDesktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        const string loadup_movies = "https://yts.mx/api/v2/list_movies.json?limit=50";
+        const string loadup_SuggestionURL = "https://yts.mx/api/v2/movie_suggestions.json";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var currentMovies = _download_serialized_json_data<Root>(loadup_movies);
+            var MoviesList = currentMovies.Data.Movies;
+            MovieListBox.ItemsSource = MoviesList;
+
+
+
+          // foreach (var movie in currentMovies.Data.Movies)
+          //  {
+          //      MessageBox.Show(movie.Title);
+          //  }
+
+        }
+
+        private static T _download_serialized_json_data<T>(string url) where T : new()
+        {
+            using (var w = new WebClient())
+            {
+                var json_data = string.Empty;
+                // attempt to download JSON data as a string
+                try
+                {
+                    json_data = w.DownloadString(url);
+                }
+                catch (Exception) { }
+                // if string with JSON data is not empty, deserialize it to class and return its instance 
+                return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
+            }
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            // for .NET Core you need to add UseShellExecute = true
+            // see https://docs.microsoft.com/dotnet/api/system.diagnostics.processstartinfo.useshellexecute#property-value
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+    }
+}
